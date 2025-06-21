63771003151942f5dd24b71154f3015f3257f8ea
moshaosama <mohamedOSFekry@gmail.com>
2025-06-21 05:06:54 +0300

    added: Hero molecule component

---
+++ src/Components/molecules/Hero.tsx
@@ -0,0 +1,30 @@
+import BoxImage from "../Atomic/BoxImage";
+import ButtonTags from "../Atomic/ButtonTags";
+import { TextAnimation } from "../Atomic/TextAnimation";
+
+const Hero = () => {
+  return (
+    <div className="mx-10 ">
+      <BoxImage
+        ImageURL="charlesdeluvio-cZr2sgaxy3Q-unsplash-113acabc.webp"
+        Width="100%"
+        Height="45pc"
+        isText={true}
+      >
+        <div className="flex flex-col gap-10">
+          <ButtonTags Title="Web Development" BackGround="black" />
+          <div>
+            <TextAnimation Title="Building Progressive Web Apps: Bridging the Gap Between Web and Mobile" />
+            <p className="text-2xl">
+              Integrating mindfulness practices helps developers cultivate
+              present-moment awareness, fostering focus, problem-solving, and
+              work-life balance.
+            </p>
+          </div>
+        </div>
+      </BoxImage>
+    </div>
+  );
+};
+
+export default Hero;
