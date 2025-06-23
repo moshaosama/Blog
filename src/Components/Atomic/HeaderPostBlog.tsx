d0c130407970fca6fa3b7815716e05fb94d97b37
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 08:47:49 +0300

    added: HeaderPostBlog atomic component

---
+++ src/Components/Atomic/HeaderPostBlog.tsx
@@ -0,0 +1,9 @@
+const HeaderPostBlog = ({ Title }: { Title: string }) => {
+  return (
+    <div>
+      <h1 className="text-[#7b00d3]">{Title}</h1>
+    </div>
+  );
+};
+
+export default HeaderPostBlog;
