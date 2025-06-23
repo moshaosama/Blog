d62c34fceb9b8135f2adb198c00a2a3b862adee7
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:29 +0300

    added: TextFooter component for footer section

---
+++ src/Components/Atomic/TextFooter.tsx
@@ -0,0 +1,15 @@
+const TextFooter = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Interesting Stories | Updates | Guides
+      </h1>
+      <p className="text-xl">
+        Subscribe to learn about new technology and updates. Join over 5000+
+        members community to stay up to date with latest news.
+      </p>
+    </div>
+  );
+};
+
+export default TextFooter;
