99bd1a4189c2de366d841d81b01a1849497cf29f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 09:42:24 +0300

    modified: TextAnimation component update

--- src/Components/Atomic/TextAnimation.tsx
+++ src/Components/Atomic/TextAnimation.tsx
@@ -1,12 +1,20 @@
+import { useTheme } from "../../Context/ThemeContext";
+import { cn } from "../../lib/cn";
 import type { TextAnimationProps } from "../../Types/TextAnimation.type";
 
 export const TextAnimation = ({ Title, fontSize }: TextAnimationProps) => {
+  const { theme } = useTheme();
   return (
     <div className="group cursor-pointer">
       <h1 className="font-bold" style={{ fontSize: fontSize }}>
         {Title}
       </h1>
-      <hr className="h-2 transition-all duration-1000 w-0 group-hover:w-full mt-1 rounded-xl border-none bg-[#7b00d3] " />
+      <hr
+        className={cn(
+          "h-2 transition-all duration-1000 w-0 group-hover:w-full mt-1 rounded-xl border-none",
+          theme === "light" ? "bg-[#7b00d3]" : "bg-[#ffdb4d]"
+        )}
+      />
     </div>
   );
 };
