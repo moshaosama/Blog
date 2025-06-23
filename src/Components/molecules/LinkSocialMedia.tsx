50f9b76f319b33e60c7ac2fc7535784bf6978bfd
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 09:42:24 +0300

    modified: LinkSocialMedia molecule

--- src/Components/molecules/LinkSocialMedia.tsx
+++ src/Components/molecules/LinkSocialMedia.tsx
@@ -1,14 +1,49 @@
 import { FaLinkedin } from "react-icons/fa";
 import LinkHeader from "../Atomic/LinkHeader";
 import { FaDribbble, FaGithub, FaTwitter } from "react-icons/fa6";
+import { cn } from "../../lib/cn";
+import { useTheme } from "../../Context/ThemeContext";
 
 const LinkSocialMedia = () => {
+  const { theme } = useTheme();
   return (
     <div className="flex gap-4">
-      <LinkHeader children={<FaLinkedin size={30} />} To="" />
-      <LinkHeader children={<FaGithub size={30} />} To="" />
-      <LinkHeader children={<FaTwitter size={30} />} To="" />
-      <LinkHeader children={<FaDribbble size={30} />} To="" />
+      <LinkHeader
+        children={
+          <FaLinkedin
+            size={30}
+            className={cn(theme === "light" ? "text-black" : "text-white")}
+          />
+        }
+        To=""
+      />
+      <LinkHeader
+        children={
+          <FaGithub
+            size={30}
+            className={cn(theme === "light" ? "text-black" : "text-white")}
+          />
+        }
+        To=""
+      />
+      <LinkHeader
+        children={
+          <FaTwitter
+            size={30}
+            className={cn(theme === "light" ? "text-black" : "text-white")}
+          />
+        }
+        To=""
+      />
+      <LinkHeader
+        children={
+          <FaDribbble
+            size={30}
+            className={cn(theme === "light" ? "text-black" : "text-white")}
+          />
+        }
+        To=""
+      />
     </div>
   );
 };
