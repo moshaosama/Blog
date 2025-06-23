d8189fc7f74ffa7ba5a2042fd0dc5f472f768859
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 08:47:43 +0300

    modified: Home page

---
+++ src/Components/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import Hero from "../molecules/Hero";
+import FeaturePosts from "../organisms/FeaturePosts";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <FeaturePosts />
+    </>
+  );
+};
+
+export default Home;
