bfd3ff8aea74bfcac727959a80699ec9ffc65d0f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 09:42:25 +0300

    modified: NavbarLinks component

--- src/Components/molecules/NavbarLinks.tsx
+++ src/Components/molecules/NavbarLinks.tsx
@@ -1,12 +1,16 @@
+import ToggleThemeFactory from "../../Utils/ToggleThemeFactory";
 import LinkHeader from "../Atomic/LinkHeader";
 
 const NavbarLinks = () => {
   return (
     <div className="fixed w-full flex justify-center z-50 ">
-      <div className="flex gap-4 border-1 border-black px-10 bg-[#ffffff80] py-3 rounded-full">
+      <div className="flex items-center gap-4 border-1  border-black px-10 bg-[#ffffff80] py-3 rounded-full">
         <LinkHeader children="Home" To="/" />
         <LinkHeader children="About" To="/about" />
         <LinkHeader children="Contact" To="/contact" />
+        <div>
+          <ToggleThemeFactory />
+        </div>
       </div>
     </div>
   );
