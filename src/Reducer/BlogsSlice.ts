f24cc7f5ae48505eb68df280328a29482d2abd8f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:31 +0300

    added: reducers folder for state management

---
+++ src/Reducer/BlogsSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchGetBlogs } from "../Actions/GetBlogs";
+
+interface initialState {
+  Blogs: {
+    result: {
+      category: string;
+      date: string;
+      id: number;
+      image: string;
+      tags: string;
+      title: string;
+    }[];
+    statusbar: string;
+  };
+  loading: boolean;
+  error: string;
+}
+
+const initialState: initialState = {
+  Blogs: {
+    result: [],
+    statusbar: "",
+  },
+  loading: false,
+  error: "",
+};
+
+const blogSlice = createSlice({
+  name: "Blogs",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchGetBlogs.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchGetBlogs.fulfilled, (state, action) => {
+      state.loading = false;
+      state.Blogs = action.payload;
+    });
+    builder.addCase(fetchGetBlogs.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload! as string;
+    });
+  },
+});
+
+export default blogSlice.reducer;
