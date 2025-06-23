e01c267190769e827ab287e0d254dcef99083467
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 08:47:44 +0300

    modified: NavbarLinks molecule

--- src/Components/molecules/NavbarLinks.tsx
+++ src/Components/molecules/NavbarLinks.tsx
@@ -2,10 +2,12 @@ import LinkHeader from "../Atomic/LinkHeader";
 
 const NavbarLinks = () => {
   return (
-    <div className="flex gap-4 border-1 border-black px-10 py-3 rounded-full">
-      <LinkHeader children="Home" To="/" />
-      <LinkHeader children="About" To="/about" />
-      <LinkHeader children="Contact" To="/contact" />
+    <div className="fixed w-full flex justify-center z-50 ">
+      <div className="flex gap-4 border-1 border-black px-10 bg-[#ffffff80] py-3 rounded-full">
+        <LinkHeader children="Home" To="/" />
+        <LinkHeader children="About" To="/about" />
+        <LinkHeader children="Contact" To="/contact" />
+      </div>
     </div>
   );
 };
