668d53a605f3c625769278af6b295de51ff3d509
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 09:42:24 +0300

    modified: HeaderPostBlog component

--- src/Components/Atomic/HeaderPostBlog.tsx
+++ src/Components/Atomic/HeaderPostBlog.tsx
@@ -1,7 +1,15 @@
+import { useTheme } from "../../Context/ThemeContext";
+import { cn } from "../../lib/cn";
+
 const HeaderPostBlog = ({ Title }: { Title: string }) => {
+  const { theme } = useTheme();
   return (
     <div>
-      <h1 className="text-[#7b00d3]">{Title}</h1>
+      <h1
+        className={cn(theme === "light" ? "text-[#7b00d3]" : "text-[#ffdb4d]")}
+      >
+        {Title}
+      </h1>
     </div>
   );
 };
