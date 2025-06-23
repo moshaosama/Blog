ae3283fb48ebb60434ca43e60b1deda12fa23c55
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:30 +0300

    added: Form molecule component

---
+++ src/Components/molecules/Form.tsx
@@ -0,0 +1,17 @@
+import InputForm from "../Atomic/InputForm";
+import Button from "../Atomic/Button";
+import LinkSocialMedia from "./LinkSocialMedia";
+
+const Form = () => {
+  return (
+    <form
+      action=""
+      className="bg-black rounded-xl py-3 w-96 flex gap-2 justify-between px-4"
+    >
+      <InputForm />
+      <Button />
+    </form>
+  );
+};
+
+export default Form;
