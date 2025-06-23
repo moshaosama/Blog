e38d3a4fa25e7bfc61fcd763ed3e767ab32b70c2
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:29 +0300

    added: InputForm field component

---
+++ src/Components/Atomic/InputForm.tsx
@@ -0,0 +1,11 @@
+const InputForm = () => {
+  return (
+    <input
+      type="text"
+      placeholder="Enter your email"
+      className="border-2 border-b-gray-50 w-96 px-2"
+    />
+  );
+};
+
+export default InputForm;
