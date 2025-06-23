8606b45278b57b0885d12770407dc534ede3878f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 08:47:44 +0300

    modified: Hero molecule

--- src/Components/molecules/Hero.tsx
+++ src/Components/molecules/Hero.tsx
@@ -4,17 +4,20 @@ import { TextAnimation } from "../Atomic/TextAnimation";
 
 const Hero = () => {
   return (
-    <div className="mx-10 ">
+    <div className="mx-10">
       <BoxImage
         ImageURL="charlesdeluvio-cZr2sgaxy3Q-unsplash-113acabc.webp"
         Width="100%"
-        Height="45pc"
+        Height="50pc"
         isText={true}
       >
         <div className="flex flex-col gap-10">
           <ButtonTags Title="Web Development" BackGround="black" />
           <div>
-            <TextAnimation Title="Building Progressive Web Apps: Bridging the Gap Between Web and Mobile" />
+            <TextAnimation
+              fontSize="2pc"
+              Title="Building Progressive Web Apps: Bridging the Gap Between Web and Mobile"
+            />
             <p className="text-2xl">
               Integrating mindfulness practices helps developers cultivate
               present-moment awareness, fostering focus, problem-solving, and
