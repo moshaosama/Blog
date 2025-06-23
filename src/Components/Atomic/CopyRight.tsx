7bbf5585567ff3ee89190795d9358b1783d0ac25
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 18:21:01 +0300

    modified: updated CopyRight text logic

--- src/Components/Atomic/CopyRight.tsx
+++ src/Components/Atomic/CopyRight.tsx
@@ -2,10 +2,12 @@ const CopyRight = () => {
   return (
     <div className="w-full">
       <hr className="h-1 border-none bg-white" />
-      <div className="flex justify-between w-full ml-10 ">
-        <h1 className="ml-10">©2023 CodeBucks. All rights reserved.</h1>
+      <div className="flex max-sm:flex-col max-sm:items-center max-sm:gap-2 justify-between w-full ml-10 max-sm:ml-0">
+        <h1 className="ml-10 max-sm:ml-0">
+          ©2023 CodeBucks. All rights reserved.
+        </h1>
         <p className="underline">sitemap.xml</p>
-        <p className="mr-36">
+        <p className="mr-36 max-sm:mr-0">
           Made with ♥ by <span>Mo4aBlogs</span>
         </p>
       </div>
