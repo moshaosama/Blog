8d65f2faaeb657f00476b3f62ca0c3f487cf6c5a
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:29 +0300

    added: CopyRight component for footer

---
+++ src/Components/Atomic/CopyRight.tsx
@@ -0,0 +1,16 @@
+const CopyRight = () => {
+  return (
+    <div className="w-full">
+      <hr className="h-1 border-none bg-white" />
+      <div className="flex justify-between w-full ml-10 ">
+        <h1 className="ml-10">©2023 CodeBucks. All rights reserved.</h1>
+        <p className="underline">sitemap.xml</p>
+        <p className="mr-36">
+          Made with ♥ by <span>Mo4aBlogs</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CopyRight;
