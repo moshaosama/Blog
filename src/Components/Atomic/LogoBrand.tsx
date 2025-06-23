a6c72a378307dab16ae311d04ff381de2d96ea05
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 09:42:24 +0300

    modified: LogoBrand component

--- src/Components/Atomic/LogoBrand.tsx
+++ src/Components/Atomic/LogoBrand.tsx
@@ -1,12 +1,21 @@
+import { useTheme } from "../../Context/ThemeContext";
+import { cn } from "../../lib/cn";
+
 export const LogoBrand = () => {
+  const { theme } = useTheme();
   return (
     <div className="flex items-center gap-2">
       <img
         src="profile-img.webp"
         alt="profileImage.png"
-        className="w-16 border-1 border-black rounded-full"
+        className={cn(
+          "w-16 border-1 rounded-full",
+          theme === "light" ? "border-black" : "border-white"
+        )}
       />
-      <h1 className="text-xl font-bold">Mo4aBlogs</h1>
+      <h1 className={cn(theme === "light" ? "text-black" : "text-white")}>
+        Mo4aBlogs
+      </h1>
     </div>
   );
 };
