1b2b97599cdca8b60f8da766d5ff9249e9e69da5
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 18:21:02 +0300

    modified: tweaked TextFooter content

--- src/Components/Atomic/TextFooter.tsx
+++ src/Components/Atomic/TextFooter.tsx
@@ -1,10 +1,10 @@
 const TextFooter = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">
+      <h1 className="text-3xl font-bold text-center max-sm:text-2xl">
         Interesting Stories | Updates | Guides
       </h1>
-      <p className="text-xl">
+      <p className="text-xl max-sm:text-center max-sm:text-sm max-sm:mt-5">
         Subscribe to learn about new technology and updates. Join over 5000+
         members community to stay up to date with latest news.
       </p>
