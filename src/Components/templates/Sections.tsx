d742e87b361c36c8b9167e3bb8d328c5aadef185
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 18:21:05 +0300

    modified: updated Sections composition

--- src/Components/templates/Sections.tsx
+++ src/Components/templates/Sections.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import HeaderForSection from "../Atomic/HeaderForSection";
 import type { SectionsProps } from "../../Types/Sections";
+import MediaQueryStyle from "../../Modules/MediaQuery.module.css";
+import { cn } from "../../lib/cn";
 
 const Sections = ({ GridColumns, Title, children }: SectionsProps) => {
+  const { section } = MediaQueryStyle;
   return (
     <section>
       <HeaderForSection Title={Title} />
       <div
-        className="grid gap-y-10"
+        className={cn("grid gap-y-10", section)}
         style={{ gridTemplateColumns: `repeat(${GridColumns}, 1fr)` }}
       >
         {React.Children.map(children, (child: any) => {
