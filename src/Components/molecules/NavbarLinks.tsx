9459c1af912e63c814cee0296c4161b3039b32c6
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 18:21:03 +0300

    modified: updated navbar routing

--- src/Components/molecules/NavbarLinks.tsx
+++ src/Components/molecules/NavbarLinks.tsx
@@ -1,10 +1,22 @@
+import { useOpenSidebar } from "../../Context/OpenSidebar";
+import { cn } from "../../lib/cn";
 import ToggleThemeFactory from "../../Utils/ToggleThemeFactory";
 import LinkHeader from "../Atomic/LinkHeader";
 
 const NavbarLinks = () => {
+  const { isOpen } = useOpenSidebar();
   return (
-    <div className="fixed w-full flex justify-center z-50 ">
-      <div className="flex items-center gap-4 border-1  border-black px-10 bg-[#ffffffe3] py-3 rounded-full">
+    <div
+      className={cn(
+        "fixed w-full flex justify-center z-50 transition-all duration-700 "
+      )}
+    >
+      <div
+        className={cn(
+          "flex items-center gap-4 border-1 max-sm:w-64 w-full border-black max-sm:px-6 px-10 max-sm:mr-10 bg-[#ffffffe3] py-3 rounded-full",
+          isOpen ? "" : "hidden"
+        )}
+      >
         <LinkHeader children="Home" To="/" />
         <LinkHeader children="About" To="/about" />
         <LinkHeader children="Contact" To="/contact" />
