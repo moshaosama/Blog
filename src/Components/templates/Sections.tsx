50c0e4fad9add72ceb61ddff5e382500dc38a53d
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 08:47:50 +0300

    added: Sections template component

---
+++ src/Components/templates/Sections.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HeaderForSection from "../Atomic/HeaderForSection";
+import type { SectionsProps } from "../../Types/Sections";
+
+const Sections = ({ GridColumns, Title, children }: SectionsProps) => {
+  return (
+    <section>
+      <HeaderForSection Title={Title} />
+      <div
+        className="grid"
+        style={{ gridTemplateColumns: `repeat(${GridColumns}, 1fr)` }}
+      >
+        {React.Children.map(children, (child: any) => {
+          return React.cloneElement(child);
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sections;
