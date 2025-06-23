fc71c11897c7063c8eb56b57ba8c4e931ebe43f7
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:11 +0300

    modified: entry point adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+
 import ThemeProvider from "./Context/ThemeContext.tsx";
+import { Store } from "./Store/store.ts";
 
 // #1b1b1b
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <Provider store={Store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 );
