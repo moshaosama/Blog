916ba4177984d87ca5355a0d4b30d1737b9908b0
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 18:21:00 +0300

    modified: BoxImage component layout adjustment

--- src/Components/Atomic/BoxImage.tsx
+++ src/Components/Atomic/BoxImage.tsx
@@ -1,5 +1,6 @@
 import { cn } from "../../lib/cn";
 import type { BoxImageProps } from "../../Types/BoxImage.type";
+import MediaQueryStyle from "../../Modules/MediaQuery.module.css";
 
 const BoxImage = ({
   ImageURL,
@@ -8,22 +9,29 @@ const BoxImage = ({
   isText,
   Height,
 }: BoxImageProps) => {
+  const { BoxImage } = MediaQueryStyle;
   return (
     <div>
       <div className="relative overflow-hidden">
         <img
           src={ImageURL}
           alt="Image.png"
-          className={cn(`object-cover rounded-2xl`)}
+          className={cn(
+            `object-cover rounded-2xl max-sm:w-full max-sm:h-full`,
+            BoxImage
+          )}
           style={{ width: Width, height: Height }}
         />
         {isText && (
           <div
-            className="absolute top-0 bg-[#00000070] rounded-2xl h-full"
+            className={cn(
+              "absolute top-0 bg-[#00000070] rounded-2xl h-full",
+              BoxImage
+            )}
             style={{ width: Width }}
           ></div>
         )}
-        <div className="absolute bottom-20 left-20">
+        <div className="absolute bottom-20 max-sm:bottom-2 left-20 max-sm:left-3">
           {isText && <div className="text-white">{children}</div>}
         </div>
       </div>
