d6b449a561fc531941606873440eb1825b6824e5
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:09 +0300

    modified: updated animation text behavior

--- src/Components/Atomic/TextAnimation.tsx
+++ src/Components/Atomic/TextAnimation.tsx
@@ -9,12 +9,12 @@ export const TextAnimation = ({ Title, fontSize }: TextAnimationProps) => {
       <h1 className="font-bold" style={{ fontSize: fontSize }}>
         {Title}
       </h1>
-      <hr
+      <span
         className={cn(
-          "h-2 transition-all duration-1000 w-0 group-hover:w-full mt-1 rounded-xl border-none",
+          "h-1 transition-all block duration-1000 w-0 group-hover:w-full mt-1 rounded-xl border-none",
           theme === "light" ? "bg-[#7b00d3]" : "bg-[#ffdb4d]"
         )}
-      />
+      ></span>
     </div>
   );
 };
