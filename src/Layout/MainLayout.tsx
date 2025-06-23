2a6355d85df7f9a1f8ad0a2608e790d68dafc54e
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 09:42:25 +0300

    modified: MainLayout structure

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,12 +1,20 @@
 import { Outlet } from "react-router";
 import Header from "../Components/organisms/Header";
+import { cn } from "../lib/cn";
+import { useTheme } from "../Context/ThemeContext";
 
 const MainLayout = () => {
+  const { theme } = useTheme();
   return (
-    <>
+    <div
+      className={cn(
+        theme === "light" ? "bg-white" : "bg-[#1b1b1b]",
+        "transition-all duration-500"
+      )}
+    >
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 };
 
