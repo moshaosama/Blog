d3df03a18436e6f189f086362367b7cd8420daac
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:09 +0300

    modified: Home page component

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "../molecules/Hero";
 import FeaturePosts from "../organisms/FeaturePosts";
+import RecentlyPosts from "../organisms/RecentlyPosts";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturePosts />
+      <RecentlyPosts />
     </>
   );
 };
