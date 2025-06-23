aef6e21462cd31ea3e9d3528f3174e6fafef2b99
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 15:35:10 +0300

    modified: FeaturePosts layout and structure

--- src/Components/organisms/FeaturePosts.tsx
+++ src/Components/organisms/FeaturePosts.tsx
@@ -1,16 +1,13 @@
 import HeaderForSection from "../Atomic/HeaderForSection";
 import { TextAnimation } from "../Atomic/TextAnimation";
-import Article_Feature_Posts from "../molecules/Featured_Posts";
+import Article_Feature_Posts from "../molecules/Article_Feature_Posts";
 import PostDetailInfo from "../molecules/PostDetailInfo";
 import Sections from "../templates/Sections";
 
 const FeaturePosts = () => {
   return (
     <>
-      <Sections
-        GridColumns={2}
-        Title={<HeaderForSection Title="Featured Posts" />}
-      >
+      <Sections GridColumns={2} Title={"Featured Posts"}>
         <Article_Feature_Posts />
         <div className="flex flex-col justify-between">
           <PostDetailInfo
@@ -29,16 +26,14 @@ const FeaturePosts = () => {
             }
           />
           <PostDetailInfo
-            Header="productivity"
+            Header="code quality"
             Height="15pc"
             Width="fit-content"
-            ImageUrl="emile-perron-xrVDYZRGdw4-unsplash-850d2f93.webp"
+            ImageUrl="roman-synkevych-vXInUOv1n84-unsplash-133222a0 (1).webp"
             flexDirection="row"
             textAnimation={
               <TextAnimation
-                Title={
-                  "Automating Repetitive Tasks: Productivity Hacks for Developers"
-                }
+                Title={"Best Practices for Writing Clean and Maintainable Code"}
                 fontSize="20px"
               />
             }
