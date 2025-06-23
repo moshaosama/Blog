af71dd0ce5d27c2752fe6c8955f0c45263a2312a
moshaosama <mohamedOSFekry@gmail.com>
2025-06-23 18:21:04 +0300

    modified: Header structure update

--- src/Components/organisms/Header.tsx
+++ src/Components/organisms/Header.tsx
@@ -1,13 +1,24 @@
+import { CgMenu } from "react-icons/cg";
 import { LogoBrand } from "../Atomic/LogoBrand";
 import LinkSocialMedia from "../molecules/LinkSocialMedia";
 import NavbarLinks from "../molecules/NavbarLinks";
+import { useOpenSidebar } from "../../Context/OpenSidebar";
+import { FaX } from "react-icons/fa6";
 
 const Header = () => {
+  const { handleOpenSidebar, isOpen, handleCloseSidebar } = useOpenSidebar();
   return (
-    <header className="flex justify-between items-center mx-10 py-5">
+    <header className="flex justify-between items-center mx-10 max-sm:mx-3 py-5">
       <LogoBrand />
       <NavbarLinks />
       <LinkSocialMedia />
+      <div className="sm:hidden">
+        {isOpen ? (
+          <FaX size={32} onClick={handleCloseSidebar} />
+        ) : (
+          <CgMenu size={32} onClick={handleOpenSidebar} />
+        )}
+      </div>
     </header>
   );
 };
