88a7d2e4e39fb6b4500822ac6a3b87d88ce0f2e4
moshaosama <mohamedOSFekry@gmail.com>
2025-06-25 21:54:39 +0300

    added: categories data source

---
+++ src/Data/Categories.ts
@@ -0,0 +1,25 @@
+import type { CategoryData } from "../Types/Categories.type";
+
+export const Categroies: CategoryData[] = [
+  {
+    name: "#all",
+  },
+  {
+    name: "#code-quality",
+  },
+  {
+    name: "#css",
+  },
+  {
+    name: "#javascript",
+  },
+  {
+    name: "#productivity",
+  },
+  {
+    name: "#pwa",
+  },
+  {
+    name: "#web-development",
+  },
+];
