9f8280dab2b45b769e1a88fb4f5852ead2f84a8f
moshaosama <mohamedOSFekry@gmail.com>
2025-06-26 04:26:07 +0300

    added: useSetActiveElement hook for scroll tracking

--- src/Hooks/useSetActiveElement.ts
+++ src/Hooks/useSetActiveElement.ts
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router";
+
 const useSetActiveElement = () => {
-  const handleClick = (el?: React.MouseEvent<HTMLButtonElement>) => {
+  const Navigate = useNavigate();
+  const handleClick = (el?: React.MouseEvent<HTMLDivElement>, data?: any) => {
     document.querySelectorAll(".Active").forEach((el: any) => {
       el.classList.remove("Active");
     });
     el?.currentTarget.classList.add("Active");
+    Navigate(data.slice(1));
   };
 
   return { handleClick };
