521666ea8692473ff7a4d16e8c31211765c7e915
moshaosama <mohamedOSFekry@gmail.com>
2025-06-26 04:26:06 +0300

    added: new useGetBlogs hook (ts version)

--- src/Hooks/useGetBlogs.ts
+++ src/Hooks/useGetBlogs.ts
@@ -2,18 +2,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchGetBlogs } from "../Actions/GetBlogs";
 import type { AppDispatch, RootState } from "../Store/store";
+import { fetchGetBlogsByCategory } from "../Actions/GetBlogsByCategory";
+import { useParams } from "react-router";
 
 const useGetBlogs = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { Blogs, loading, error } = useSelector(
-    (state: RootState) => state.Blogs
+
+  const { category_id } = useParams();
+  const { Blogs } = useSelector((state: RootState) => state.Blogs);
+
+  const { BlogByCategory, loading: BlogByCategoryLoading } = useSelector(
+    (state: RootState) => state.BlogbyCategory
   );
 
   useEffect(() => {
     dispatch(fetchGetBlogs());
-  }, [dispatch]);
+    dispatch(fetchGetBlogsByCategory(category_id as string));
+  }, [dispatch, category_id]);
 
-  return { Blogs, loading, error };
+  return { Blogs, BlogByCategory, BlogByCategoryLoading };
 };
 
 export default useGetBlogs;
